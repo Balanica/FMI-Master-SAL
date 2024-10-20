dab3978aae286cb21076ae8ce88c0fa586607946
Balanica_Andrei <95495921+Balanica@users.noreply.github.com>
2024-10-20 22:14:15 +0300

    Update ex.3.py

--- Anomaly Detection/Laboratorul_1/ex.3.py
+++ Anomaly Detection/Laboratorul_1/ex.3.py
@@ -1,7 +1,7 @@
 from pyod.utils.data import generate_data
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, balanced_accuracy_score, accuracy_score
+from sklearn.metrics import confusion_matrix
 
 X_train, X_test, y_train, y_test = generate_data(n_train=1000, n_test=0, n_features=1, contamination=0.1)
 
@@ -30,13 +30,5 @@ tn, tp, fn, fp = confusion_matrix(y_train, prediction).ravel()
 tpr = tp / (tp + fn)
 tnr = tn / (tn + fp)
 balanced_accuracy = (tpr + tnr) / 2
-# acc = balanced_accuracy_score(y_train, prediction)
-# acc2 = accuracy_score(y_train, prediction)
-print(balanced_accuracy)
 
-# plt.plot([mean, mean], [5, -5], color="green")
-# # plt.plot([mean+threshold, mean+threshold], [5, -5], color="purple")
-# # plt.plot([-threshold+mean, -threshold+mean], [5, -5], color="purple")
-# plt.scatter(normale,len(normale) * [1], color="blue")
-# plt.scatter(anomali,len(anomali) * [1], color="red")
-# plt.show()
+print(balanced_accuracy)
